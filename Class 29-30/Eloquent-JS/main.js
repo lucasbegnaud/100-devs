7d85f1e136ce84cb20f8df9e4d9b78e044beb8ba
Lucas Begnaud <lucasbegnaud@gmail.com>
2024-12-10 15:31:48 -0700

    Eloquent Javascript Objects Homework

---
+++ Class 29-30/Eloquent-JS/main.js
@@ -0,0 +1,29 @@
+//A vector type//
+class Vector {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    plus(vec) {
+        return new Vector(this.x + vec.x, this.y + vec.y);
+    };
+    
+    minus(vec) {
+        return new Vector(this.x - vec.x, this.y - vec.y);
+    };
+
+    get length() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    };
+}
+
+console.log(new Vector(1, 2).plus(new Vector(2, 3))); // Vec { x: 3, y: 5 }
+console.log(new Vector(1, 2).minus(new Vector(2, 3))); // Vec { x: -1, y: -1 }
+console.log(new Vector(3, 4).length); // 5
+
+//Groups//
+//Code.....//
+
+//Iterable groups//
+//Code.....//
