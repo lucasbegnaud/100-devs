4ea67ab6cc3d3a980c3e9970a6f69524230ee976
Lucas Begnaud <lucasbegnaud@gmail.com>
2024-12-11 13:34:35 -0700

    Second Eloquent Javascript question answered

--- Class 29-30/Eloquent-JS/main.js
+++ Class 29-30/Eloquent-JS/main.js
@@ -23,7 +23,43 @@ console.log(new Vector(1, 2).minus(new Vector(2, 3))); // Vec { x: -1, y: -1 }
 console.log(new Vector(3, 4).length); // 5
 
 //Groups//
-//Code.....//
+class Group {
+    constructor() {
+      this.arr = [];
+    }
+  
+    static from(arr) {
+      let group = new Group();
+      group.arr = arr;
+      return group;
+    }
+  
+    has(item) {
+      for (let i = 0; i < this.arr.length; i++) {
+        if (this.arr[i] === item) {
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    add(item) {
+      if (!this.has(item)) {
+        this.arr.push(item);
+      }
+    }
+  
+    delete(item) {
+      this.arr = this.arr.filter(i => i !== item);
+    }
+  }
+  
+  let group = Group.from([10, 20]);
+  console.log(group.has(10)); // → true
+  console.log(group.has(30)); // → false
+  group.add(30);
+  group.delete(10);
+  console.log(group.has(10)); // → false
 
 //Iterable groups//
 //Code.....//
